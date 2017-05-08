5e0d863e5b361dc2979204c0ddaef15503fb47ad
JF-Lalonde <lalondej88@gmail.com>
2017-05-08 08:46:30 -0600

    refactoring

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -3,14 +3,14 @@ class InsertionSort
 # Define method for sorting through a list
   def sort(list)
     (1...list.length).each {|i|
-      sa_index = list[i]
+      sa_number = list[i]
       ua_index = i - 1
-      while ua_index >= 0 && list[ua_index] > sa_index
+      while ua_index >= 0 && list[ua_index] > sa_number
         list[ua_index + 1] = list[ua_index]
         ua_index = ua_index - 1
       end
       # Update index location
-    list[ua_index + 1] = sa_index
+    list[ua_index + 1] = sa_number
   }
   # Return sorted list as array
   puts list.inspect
